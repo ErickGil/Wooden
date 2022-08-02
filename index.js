ff1f5b65a6021180290300f2bc12559e0a92a182
ErickGil <ericksebagill@gmail.com>
2022-08-01 21:51:06 -0300

    probando fetch

--- index.js
+++ index.js
@@ -25,11 +25,10 @@ class Carrito {
     }
 
 }
-
-function Agregarproductos(id, nombre, calidad, precio, cantidad, imagen) {
+/*function Agregarproductos(id, nombre, calidad, precio, cantidad, imagen) {
     let productoNuevo = new Producto(id, nombre, calidad, precio, cantidad, imagen)
     stock.push(productoNuevo)
-}
+}*/
 
 function limpiarCarro() {
     let divCarro = document.querySelector("#carro")
@@ -96,7 +95,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     actualizarCarro(carritoGuardado);
 });
 
-Agregarproductos(1, "Escritorio", "De Diseño", 24000, 2, "escritorio.jpg")
+/*Agregarproductos(1, "Escritorio", "De Diseño", 24000, 2, "escritorio.jpg")
 Agregarproductos(2, "Mesa Comerdor", "Generico", 12000, 5, "mesa.jpg")
 Agregarproductos(3, "Mesa de Tv", "De Diseño", 15000, 3, "mesaTv.jpg")
 Agregarproductos(4, "Mesa de Tv", "Generico", 10000, 6, "mesaTvDos.jpg")
@@ -104,12 +103,12 @@ Agregarproductos(5, "Puerta", "Generico", 17000, 4, "puerta.jpg")
 Agregarproductos(6, "Silla", "Generico", 4000, 8, "silla.jpg")
 Agregarproductos(7, "Velador", "Generico", 6000, 4, "velador.jpg")
 Agregarproductos(8, "Ventana", "Generico", 15000, 4, "ventanaDos.jpg")
-Agregarproductos(9, "Ventana", "De Diseño", 20000, 1, "ventanaUno.jpg")
+Agregarproductos(9, "Ventana", "De Diseño", 20000, 1, "ventanaUno.jpg")*/
 
 
 let contenedorProductos = document.getElementById("contenedor-productos")
 
-for (const elemnt of stock) {
+/*for (const elemnt of stock) {
     let columna = document.createElement("div")
     columna.className = "col-md-4 mt-3"
     columna.id = `columna-${elemnt.id}`
@@ -126,7 +125,7 @@ for (const elemnt of stock) {
         </div>
     `
     contenedorProductos.append(columna)
-}
+}*/
 
 let carro = new Carrito(1);
 
@@ -169,3 +168,30 @@ botonLimpiar.addEventListener("click", () => {
 
 
 })
+const pedirApi = async () => {
+    const resp = await fetch('https://62e8682593938a545be58877.mockapi.io/articulos')
+    const data = await resp.json()
+
+    stock = data
+    
+    for (const elemnt of stock) {
+        let columna = document.createElement("div")
+        columna.className = "col-md-4 mt-3"
+        columna.id = `columna-${elemnt.id}`
+        columna.innerHTML = `
+            <div class="card">
+            <img class="card-img-top" src="./fotosMuebles/${elemnt.imagen}" alt="Card image">
+                <div class = "card-body">
+                    <h4 class="card-title"<b>${elemnt.nombre}</b></h4>
+                    <p class="card-text">Calidad: <b>${elemnt.calidad}</b></p>
+                    <p class="card-text">Precio: <b>$${elemnt.precio}</b></p>
+                    <p class="card-text">Cantidad: <b>${elemnt.cantidad}</b></p>
+                    <a class="btn btn-primary botonDeCompra" id= ${elemnt.id} >Agregar al carrito</a>
+                </div>
+            </div>
+        `
+        contenedorProductos.append(columna)
+    }
+}
+
+pedirApi()
